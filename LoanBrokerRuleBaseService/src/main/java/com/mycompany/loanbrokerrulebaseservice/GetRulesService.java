bd3083197abb149f71acc819fe09d8a158875882
robertelving <vivalaklask@gmail.com>
2016-12-20 22:11:45 +0100

    fixed RuleSystem

--- LoanBrokerRuleBaseService/src/main/java/com/mycompany/loanbrokerrulebaseservice/GetRulesService.java
+++ LoanBrokerRuleBaseService/src/main/java/com/mycompany/loanbrokerrulebaseservice/GetRulesService.java
@@ -22,16 +22,14 @@ public class GetRulesService {
     public String getRules() {
         
         StringBuilder sb = new StringBuilder();
-        sb.append("<ruleobject>");
-        sb.append("<rules>");
-            sb.append("<rule><min>0</min><max>500</max><banks><bank><bankNo>3</bankNo></bank><bank><bankNo>4</bankNo></bank></banks></rule>");
-            sb.append("<rule><min>580</min><max>619</max><banks><bank><bankNo>3</bankNo></bank><bank><bankNo>4</bankNo></bank></banks></rule>");
-            sb.append("<rule><min>580</min><max>619</max><banks><bank><bankNo>3</bankNo></bank><bank><bankNo>4</bankNo></bank></banks></rule>");
-            sb.append("<rule><min>620</min><max>679</max><banks><bank><bankNo>2</bankNo></bank><bank><bankNo>3</bankNo></bank></banks></rule>");
-            sb.append("<rule><min>680</min><max>719</max><banks><bank><bankNo>2</bankNo></bank><bank><bankNo>3</bankNo></bank></banks></rule>");
-            sb.append("<rule><min>720</min><max>800</max><banks><bank><bankNo>1</bankNo></bank><bank><bankNo>2</bankNo></bank><bank><bankNo>3</bankNo></bank></banks></rule>");
-        sb.append("</rules>");
-        sb.append("</ruleobject>");
+        sb.append("<RuleObject>");
+            sb.append("<rule><min>0</min><max>500</max><bank><bankno>3</bankno></bank><bank><bankno>4</bankno></bank></rule>");
+            sb.append("<rule><min>580</min><max>619</max><bank><bankno>3</bankno></bank><bank><bankno>4</bankno></bank></rule>");
+            sb.append("<rule><min>580</min><max>619</max><bank><bankno>3</bankno></bank><bank><bankno>4</bankno></bank></rule>");
+            sb.append("<rule><min>620</min><max>679</max><bank><bankno>2</bankno></bank><bank><bankno>3</bankno></bank></rule>");
+            sb.append("<rule><min>680</min><max>719</max><bank><bankno>2</bankno></bank><bank><bankno>3</bankno></bank></rule>");
+            sb.append("<rule><min>720</min><max>800</max><bank><bankno>1</bankno></bank><bank><bankno>2</bankno></bank><bank><bankno>3</bankno></bank></rule>");
+        sb.append("</RuleObject>");
                 
         return sb.toString();
    
